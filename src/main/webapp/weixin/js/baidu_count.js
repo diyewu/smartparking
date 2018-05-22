c4d9431c95fbae868de5f39c0211bfc0e4e6ff17
diyewu <930725713@qq.com>
2018-05-22 18:14:28 +0800

    0522

---
+++ src/main/webapp/weixin/js/baidu_count.js
@@ -0,0 +1,10 @@
+/**
+ * Created by chengWang on 2016/12/7.
+ */
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "hm.js-4d414a00214b258efbaf568d27a4dc23"/*tpa=https://hm.baidu.com/hm.js?4d414a00214b258efbaf568d27a4dc23*/;
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
